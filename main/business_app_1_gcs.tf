77eeef6203d3bb80fdf93eda50ebcff3c8219e4e
mybabygrand <amuro_ray@kakao.com>
2024-08-30 22:46:01 +0900

    add gcs for start-script

---
+++ main/business_app_1_gcs.tf
@@ -0,0 +1,32 @@
+# GCS bucket
+resource "google_storage_bucket" "business_app_1_bucket" {
+  project                     = google_project.proj_infra_bapp1_prod_0.project_id
+  name                        = "gcs-${google_project.proj_infra_bapp1_prod_0.project_id}-bapp1-scripts"
+  location                    = "US"
+  storage_class               = "STANDARD"
+  public_access_prevention    = "enforced"
+  uniform_bucket_level_access = true
+
+  depends_on = [
+    google_project.proj_infra_bapp1_prod_0
+  ]
+}
+
+# resource "time_sleep" "delay_create_30s" {
+#   depends_on = [
+#     google_storage_bucket.business_app_1_bucket
+#   ]
+#   create_duration = "30s"
+# }
+
+# Startup script object
+resource "google_storage_bucket_object" "business_app_1_app_startup_script" {
+  name = "scripts/startup-script.sh"
+  #source = "scripts/bapp-1/startup-script.sh"
+  content = file("scripts/bapp-1/startup-script.sh")
+  bucket  = google_storage_bucket.business_app_1_bucket.id
+
+  #   depends_on = [
+  #     time_sleep.delay_create_30s
+  #   ]
+}
---
+++ main/scripts/bapp-1/startup-script.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+apt-get update -y
+apt-get install -yq build-essential python3-pip rsync
+pip install flask
+mkdir -p /app
+cat <<EOF > /app/main.py
+#
+# This example is taken from: https://www.geeksforgeeks.org/flask-creating-first-simple-application/
+#
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask
+ 
+# Flask constructor takes the name of
+# current module (__name__) as argument.
+app = Flask(__name__)
+ 
+# The route() function of the Flask class is a decorator,
+# which tells the application which URL should call
+# the associated function.
+@app.route('/')
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+    return 'Hello World - This is a new version v2'
+ 
+# main driver function
+if __name__ == '__main__':
+ 
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run(host='0.0.0.0', port=8888)
+EOF
+python3 /app/main.py
